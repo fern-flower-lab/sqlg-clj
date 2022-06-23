5f67032fa390ec92bd19f7a0620ec5140ba36613
MelKori <mieldo@gmail.com>
2022-06-23 17:59:41 +0300

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ai.z7/sqlg-clj "0.0.2"
+(defproject ai.z7/sqlg-clj "0.0.3"
   :description "The SQL Graph with Tinkerpop3 and Clojure"
   :url "https://github.com/fern-flower-lab/sqlg-clj"
   :license {:name "MIT"}
