51a462ef94f188d23ba08df728442c1fe7437ae1
MelKori <mieldo@gmail.com>
2023-08-10 14:25:59 +0300

    set tgt as v9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ai.z7/sqlg-clj "0.0.6"
+(defproject ai.z7/sqlg-clj "0.0.7"
   :description "The SQL Graph with Tinkerpop3 and Clojure"
   :url "https://github.com/fern-flower-lab/sqlg-clj"
   :license {:name "MIT"}
@@ -24,7 +24,7 @@
                  [org.umlg/sqlg-c3p0 "3.0.1"
                   :exclusions [com.google.guava/guava]]]
   :source-paths ["src-clj"]
-  #_#_ :java-source-paths ["src-java"]
+  #_#_:java-source-paths ["src-java"]
   :profiles {:dev     {:global-vars  {*assert* true}
                        :dependencies [[ch.qos.logback/logback-classic "1.4.11"
                                        :exclusions [org.slf4j/slf4j-api]]
@@ -32,8 +32,9 @@
                                       [org.slf4j/jcl-over-slf4j "2.0.7"]
                                       [org.slf4j/log4j-over-slf4j "2.0.7"]
                                       [org.clojure/tools.logging "1.2.4"]]
-                       #_#_:junit        ["src-java"]}
+                       #_#_:junit ["src-java"]}
              :uberjar {:aot :all}}
+  :provided {:javac-options ["-source" "9" "-target" "9" "-g:none"]}
   :plugins [[lein-junit "1.1.9"]]
   :global-vars {*warn-on-reflection* true
                 *assert*             false})
